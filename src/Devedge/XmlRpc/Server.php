f75f8e9a58dfc2afc57ea5de85b26de4874be05c
Peter Petermann <peter.petermann@gameforge.com>
2014-09-08 00:26:45 +0200

    Release of new version 0.1.0

--- src/Devedge/XmlRpc/Server.php
+++ src/Devedge/XmlRpc/Server.php
@@ -19,7 +19,7 @@ class Server implements LoggerAwareInterface
     /**
      * @var string Version string (to be updated by RMT, dont change manually)
      */
-    public static $version = "0.0.0";
+    public static $version = "0.1.0";
 
     /**
      * @var bool
